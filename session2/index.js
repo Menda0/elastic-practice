611f76e7793a27be6959f615c0b2c8cc623cfd1f
Marco Mendão <mac.mendao@gmail.com>
2021-02-17 20:15:18 +0000

    Add update document

--- session2/index.js
+++ session2/index.js
@@ -29,10 +29,53 @@ function createDocument(){
             name:"rating",
             message:"Rating:"
         }
-    ]).then(function (answers){
+    ]).then(async function (answers){
         const {title, author, rating} = answers;
-        elasticCreateDocument(title, author, rating);
+        await elasticCreateDocument(title, author, rating);
+        run();
     }).catch()
+}
+
+async function elasticUpdateDocument(id, title, author, rating) {
+    const url = `${ELASTIC_URL}/${INDEX}/_doc/${id}`;
+    const body = {title, author, rating};
+
+    const {data} = await axios.put(url, body);
+    console.log(data);
+}
+
+function updateDocument(){
+    // We need id, title, author and rating of the book
+    // We need to make a put operation to elastic search
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "id",
+            message: "Document Id:"
+        },
+        {
+            type:"input",
+            name:"title",
+            message:"Title:"
+        },
+        {
+            type:"input",
+            name:"author",
+            message:"Author:"
+        },
+        {
+            type:"number",
+            name:"rating",
+            message:"Rating:"
+        }
+    ]).then(async function (answers) {
+        const {title, author, rating, id} = answers;
+        await elasticUpdateDocument(id, title, author, rating);
+        run();
+    }).catch(function(err) {
+        console.log(err);
+    })
+
 }
 
 function run(){
@@ -41,17 +84,15 @@ function run(){
             type:"rawlist",
             name:"action",
             message:"Action?",
-            choices: ["Create Document", "Update Document","Get Document", "Delete Document"]
+            choices: ["Create Document", "Update Document","Get Document", "Delete Document", "Exit"]
         }
     ]).then(function (answers){
         switch(answers['action']){
             case "Create Document":
                 createDocument();
-                console.log("create document");
                 break;
             case "Update Document":
-                // updateDocument();
-                console.log("Update Document");
+                updateDocument();
                 break;
             case "Get Document":
                 // getDocument()
@@ -61,6 +102,8 @@ function run(){
                 // deleteDocument();
                 console.log("Delete document")
                 break;
+            case "Exit":
+                return;
             default:
                 run();
                 break;
