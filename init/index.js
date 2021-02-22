c976206b9f61e74fe5a6509dd53402b356936a23
Marco Mendão <mac.mendao@gmail.com>
2021-02-22 20:00:44 +0000

    Add init script

--- init/index.js
+++ init/index.js
@@ -1,4 +1,15 @@
 const {Book} = require('../models');
+const axios = require('axios');
+
+const ELASTIC_SEARCH_ENDPOINT = "http://localhost:9200";
+const INDEX = "books2";
+
+async function addBook(jsonBook) {
+    const url = `${ELASTIC_SEARCH_ENDPOINT}/${INDEX}/_doc/${jsonBook.id}`;
+    // Can also use put
+    const {data} = await axios.post(url, jsonBook);
+    console.log(`Book added with name ${jsonBook.title}`)
+}
 
 async function elasticInit() {
     try{
@@ -6,7 +17,7 @@ async function elasticInit() {
 
         for(const book of books){
             const jsonBook = book.toJSON();
-            console.log(jsonBook);
+            await addBook(jsonBook)
         }
     }catch(e){
         console.log(e);
@@ -14,4 +25,4 @@ async function elasticInit() {
     
 }
 
-elasticInit()
+elasticInit();
