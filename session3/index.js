a7d81dcb6919d3459d3622f1e34b94118d68045b
Marco Mendão <mac.mendao@gmail.com>
2021-03-02 17:21:40 +0000

    Add changes session3

--- session3/index.js
+++ session3/index.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 
 const ELASTIC_URL = "http://localhost:9200";
 const INDEX = "books";
+const SEARCH_URL = `${ELASTIC_URL}/${INDEX}/_search`;
 
 function outputBooks(hits){
     const books = [];
@@ -12,13 +13,14 @@ function outputBooks(hits){
         // book.score = hit._score;
         // delete book.description;
 
-        const {id, title, author} = hit._source;
+        const {id, title, author, rating} = hit._source;
         const score = hit._score;
         const book = {
             id,
             title,
             author,
-            score
+            score,
+            rating
         }
 
         books.push(book);
@@ -27,10 +29,56 @@ function outputBooks(hits){
     return books;
 }
 
+// Compound query clause
+// Must -> Match operation title=${title}
+// Filter -> Range operation rating gte 3
+async function searchBookRatingGte(title, rating){
+    
+    const body = {
+        "query":{
+            "bool":{
+                "must":[
+                    {
+                        "match":{
+                            "title":title
+                        }
+                    }
+                ],
+                "filter":[
+                    {
+                        "range":{
+                            "rating":{
+                                "gte": rating
+                            }
+                        }
+                    }
+                ],
+                "should": [],
+                "must_not": []
+            }
+        },
+        "aggs":{
+            "stats_ratings":{
+                "stats":{
+                    "field":"rating"
+                }
+            }
+        }
+    }
+
+    const {data} = await axios.post(SEARCH_URL, body);
+    const {stats_ratings} = data.aggregations;
+
+    console.table(stats_ratings);
+
+    const books = outputBooks(data.hits.hits);
+
+    console.table(books);
+}
+
 // Leaf query clause
 // Match operation title=${title}
 async function searchBook(title) {
-    const SEARCH_URL = `${ELASTIC_URL}/${INDEX}/_search`;
 
     // Query DSL
     const body = {
@@ -56,12 +104,19 @@ function run(){
                 type:"input",
                 name:"title",
                 message:"Book name?"
+            },
+            {
+                type:"number",
+                name:"rating",
+                message:"Book rating?"
             }
         ]
     ).then(function (answers){
         // console.log(answers.title);
-        const title = answers.title;
-        searchBook(title);
+        const {title, rating} = answers;
+
+        //searchBook(title);
+        searchBookRatingGte(title, rating);
     }).catch(
         function name(err) {
             console.log(err);
